73e968db12c6066cdc03f7f3cb5f84d02761a192
jo se <danieldezena098@gmail.com>
2021-10-27 12:05:20 -0300

    atualização dos arquivos

--- sql_diagrama/RedeSocial_mysql_create.sql
+++ sql_diagrama/RedeSocial_mysql_create.sql
@@ -1,22 +1,23 @@
-CREATE TABLE `Postagens` (
+CREATE TABLE `tb_postagens` (
 	`id_postagem` bigint NOT NULL AUTO_INCREMENT,
 	`titulo_postagem` varchar(255) NOT NULL,
 	`texto_postagem` varchar(1000) NOT NULL,
 	`data_postagem` TIMESTAMP NOT NULL,
 	`curtidas_postagem` bigint NOT NULL AUTO_INCREMENT,
-	`fk_usuario` varchar(255) NOT NULL,
+	`fk_usuario` bigint NOT NULL,
 	`fk_tema` bigint NOT NULL,
 	PRIMARY KEY (`id_postagem`)
 );
 
-CREATE TABLE `Usuarios` (
+CREATE TABLE `tb_usuarios` (
+	`id_usuario` bigint NOT NULL AUTO_INCREMENT,
 	`email_usuario` varchar(256) NOT NULL,
 	`nome_usuario` varchar(255) NOT NULL,
 	`senha_usuario` varchar(255) NOT NULL,
-	PRIMARY KEY (`email_usuario`)
+	PRIMARY KEY (`id_usuario`)
 );
 
-CREATE TABLE `Tema` (
+CREATE TABLE `tb_tema` (
 	`id_tema` bigint NOT NULL AUTO_INCREMENT,
 	`titulo_tema` varchar(80) NOT NULL,
 	`descricao_tema` varchar(255) NOT NULL,
@@ -24,9 +25,9 @@ CREATE TABLE `Tema` (
 	PRIMARY KEY (`id_tema`)
 );
 
-ALTER TABLE `Postagens` ADD CONSTRAINT `Postagens_fk0` FOREIGN KEY (`fk_usuario`) REFERENCES `Usuarios`(`email_usuario`);
+ALTER TABLE `tb_postagens` ADD CONSTRAINT `tb_postagens_fk0` FOREIGN KEY (`fk_usuario`) REFERENCES `tb_usuarios`(`id_usuario`);
 
-ALTER TABLE `Postagens` ADD CONSTRAINT `Postagens_fk1` FOREIGN KEY (`fk_tema`) REFERENCES `Tema`(`id_tema`);
+ALTER TABLE `tb_postagens` ADD CONSTRAINT `tb_postagens_fk1` FOREIGN KEY (`fk_tema`) REFERENCES `tb_tema`(`id_tema`);
 
 
 
