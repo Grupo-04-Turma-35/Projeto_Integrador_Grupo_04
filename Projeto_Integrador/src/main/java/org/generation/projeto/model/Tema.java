650e8e0ce6f62419551390f3e4d603d80f526805
Rafael Rodrigues <raffa.dma@gmail.com>
2021-11-09 14:25:52 -0300

    Atualizações dos nomes dos atributos e da URI

--- Projeto_Integrador/src/main/java/org/generation/projeto/model/Tema.java
+++ Projeto_Integrador/src/main/java/org/generation/projeto/model/Tema.java
@@ -15,62 +15,61 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "tb_tema")
 public class Tema {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long idTema;
+	private long idtema;
 	
 	@NotBlank(message = "O título do tema é obrigatório")
 	@Size(min = 5, max = 100, message = "O título precisa conter de 5 a 100 caracteres")
-	private String tituloTema;
+	private String titulo;
 	
 	@NotBlank(message = "O texto do tema é obrigatório")
 	@Size(min = 5, max = 500, message = "O texto precisa conter de 5 a 500 caracteres")
-	private String descricaoTema;
+	private String descricao;
 	
-	private long qtdTema;
+	private long qtdtema;
 	
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date dataTema = new java.sql.Date(System.currentTimeMillis());
+	private Date datatema = new java.sql.Date(System.currentTimeMillis());
 
-	public long getIdTema() {
-		return idTema;
+	public long getIdtema() {
+		return idtema;
 	}
 
-	public void setIdTema(long idTema) {
-		this.idTema = idTema;
+	public void setIdtema(long idtema) {
+		this.idtema = idtema;
 	}
 
-	public String getTituloTema() {
-		return tituloTema;
+	public String getTitulo() {
+		return titulo;
 	}
 
-	public void setTituloTema(String tituloTema) {
-		this.tituloTema = tituloTema;
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 
-	public String getDescricaoTema() {
-		return descricaoTema;
+	public String getDescricao() {
+		return descricao;
 	}
 
-	public void setDescricaoTema(String descricaoTema) {
-		this.descricaoTema = descricaoTema;
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
-	public long getQtdTema() {
-		return qtdTema;
+	public long getQtdtema() {
+		return qtdtema;
 	}
 
-	public void setQtdTema(long qtdTema) {
-		this.qtdTema = qtdTema;
+	public void setQtdtema(long qtdtema) {
+		this.qtdtema = qtdtema;
 	}
 
-	public Date getDataTema() {
-		return dataTema;
+	public Date getDatatema() {
+		return datatema;
 	}
 
-	public void setDataTema(Date dataTema) {
-		this.dataTema = dataTema;
+	public void setDatatema(Date datatema) {
+		this.datatema = datatema;
 	}
-
 }
