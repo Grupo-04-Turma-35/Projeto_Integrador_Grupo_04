0cb21828e5a8301572790efef456c02ebb051aa2
jo se <danieldezena098@gmail.com>
2021-11-17 09:14:10 -0300

    atualização da model e repository do usuario e criação da classe usuarioLogin

--- Projeto_Integrador/src/main/java/org/generation/projeto/model/Usuario.java
+++ Projeto_Integrador/src/main/java/org/generation/projeto/model/Usuario.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -16,29 +17,41 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
-@Table(name= "tb_usuario")
+@Table(name = "tb_usuario")
 public class Usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idusuario;
-	
+
 	@NotNull(message = "O email é obrigatório")
-	@Size(min = 5, max = 100, message= "O email deve conter entre 5 e 100 caracteres. ")
+	@Email
 	private String email;
-	
+
 	@NotBlank(message = "O nome é obrigatório")
 	@Size(max = 100, message = "O nome deve conter até 100 caracteres. ")
 	private String nome;
-	
+
 	@NotNull(message = "A senha é obrigatória")
-	@Size(min = 8, max = 100, message= "A senha deve conter entre 8 e 100 caracteres. ")
+	@Size(min = 8, max = 100, message = "A senha deve conter entre 8 e 100 caracteres. ")
 	private String senha;
-	
+
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 
+	public Usuario(long idusuario, String email, String nome, String senha) {
+		
+		this.idusuario = idusuario;
+		this.email = email;
+		this.nome = nome;
+		this.senha = senha;
+
+	}
+	
+	public Usuario() {
+	}
+	
 	public long getIdusuario() {
 		return idusuario;
 	}
--- Projeto_Integrador/src/main/java/org/generation/projeto/repository/UsuarioRepository.java
+++ Projeto_Integrador/src/main/java/org/generation/projeto/repository/UsuarioRepository.java
@@ -1,10 +1,17 @@
 package org.generation.projeto.repository;
 
+import java.util.Optional;
+
 import org.generation.projeto.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	/*
+	 * método para procurar tudo a partir pelo email no atributo usuário  
+	 */
+	
+	public Optional<Usuario> findByUsuario(String usuario);
 
 }
