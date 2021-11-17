d58effb4703f3ce797a66ba02133d904de49340c
Mábile Cristina <crismabile@gmail.com>
2021-11-17 10:55:35 -0300

    Criando a classe UserDetailsServiceImpl

---
+++ Projeto_Integrador/src/main/java/org/generation/projeto/seguranca/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package org.generation.projeto.seguranca;
+
+import java.util.Optional;
+
+import org.generation.projeto.model.Usuario;
+import org.generation.projeto.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired 
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Usuario> user = usuarioRepository.findByUsuario(username);
+		user.orElseThrow(() -> new UsernameNotFoundException(username + " não encontrado."));
+		
+		return user.map(UserDetailsImpl :: new).get();
+	}
+}
